2c25cbd1057b53c9b37732df7244c3a767fe33b9
Nevin <39220032+TypicalNevin@users.noreply.github.com>
2021-05-21 13:03:53 +0200

    Add project files.

---
+++ clipsyncService/ClipSyncService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace clipsyncService
+{
+    public partial class ClipSyncService : ServiceBase
+    {
+        public ClipSyncService()
+        {
+            InitializeComponent();
+            this.eventLog1 = new EventLog();
+            if (!EventLog.SourceExists("ClipSyncSource"))
+            {
+                EventLog.CreateEventSource("ClipSyncSource", "ClipSyncLog");
+            }
+
+            eventLog1.Source = "ClipSyncSource";
+            eventLog1.Log = "ClipSyncLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Service Started");
+            Timer timer = new Timer
+            {
+                Interval = 10000
+            };
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Service Stopped");
+        }
+
+        public void OnTimer (object sender, ElapsedEventArgs args)
+        {
+            string allProcesses = "";
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                allProcesses += $"{process.ProcessName} ";
+            }
+
+            eventLog1.WriteEntry(allProcesses);
+        }
+
+    }
+}
