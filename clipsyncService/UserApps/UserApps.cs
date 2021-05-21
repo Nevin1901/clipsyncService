fce6b2ace471dc6855e95e9aa049f17c46af0524
Nevin <39220032+TypicalNevin@users.noreply.github.com>
2021-05-21 22:52:33 +0200

    cleaned up code some more

---
+++ clipsyncService/UserApps/UserApps.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using clipsyncService.UserApps;
+
+namespace clipsyncService.Models
+{
+    public class UserApps : IEnumerable
+    {
+        private IApp[] _apps;
+
+        public UserApps(IApp[] apps)
+        {
+            _apps = new IApp[apps.Length];
+            for (int i = 0; i < apps.Length; i++)
+            {
+                _apps[i] = apps[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator) GetEnumerator();
+        }
+
+        public UserAppsEnum GetEnumerator()
+        {
+            return new UserAppsEnum(_apps);
+        }
+
+        public List<string> GetAllAppNames()
+        {
+            List<string> appNames = new List<string>();
+            foreach (var app in _apps)
+            {
+                appNames = appNames.Append(app.Title).ToList();
+            }
+
+            return appNames;
+        }
+
+        public bool ContainsApp(string appName)
+        {
+            foreach (var app in _apps)
+            {
+                if (app.Title == appName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ clipsyncService/UserApps/UserAppsEnum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using clipsyncService.Models;
+
+namespace clipsyncService.UserApps
+{
+    public class UserAppsEnum : IEnumerator
+    {
+        public IApp[] _apps;
+
+        private int _position = -1;
+
+        public UserAppsEnum(IApp[] list)
+        {
+            _apps = list;
+        }
+
+        public bool MoveNext()
+        {
+            _position++;
+            return (_position < _apps.Length);
+        }
+
+        public void Reset()
+        {
+            _position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                try
+                {
+                    return _apps[_position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+    }
+}
